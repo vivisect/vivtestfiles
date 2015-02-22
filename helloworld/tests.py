ce5c494f53aebe70f36a14326e78fd03a94259e3
invisigoth <invisigoth@kenshoto.com>
2015-02-21 19:59:57 -0500

    adding helloworld binaries and initial trivial tests

--- helloworld/tests.py
+++ helloworld/tests.py
@@ -1,15 +1,18 @@
 import os
 import unittest
 
+import vivisect.lib.bexfile as v_bexfile
 import vivtestfiles.helpers as v_helpers
 
 class HelloWorldTest(unittest.TestCase):
 
     def test_helloworld_i386_exe(self):
         with v_helpers.testfd('helloworld','hello_i386.exe') as fd:
-            pass
+            bex = v_bexfile.getBexFile(fd)
+            self.assertEqual( bex.baseaddr(), 0x400000 )
 
     def test_helloworld_amd64_exe(self):
-        with v_helpers.testfd('helloworld','hello_i386.exe') as fd:
-            pass
+        with v_helpers.testfd('helloworld','hello_amd64.exe') as fd:
+            bex = v_bexfile.getBexFile(fd)
+            self.assertEqual( bex.baseaddr(), 0x140000000 )
 
