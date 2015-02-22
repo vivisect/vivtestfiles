79dcb6caa26186bf70c8780ae8493205ec54365f
invisigoth <invisigoth@kenshoto.com>
2015-02-22 18:01:39 -0500

    more workspace tests

--- helloworld/tests.py
+++ helloworld/tests.py
@@ -1,6 +1,7 @@
 import os
 import unittest
 
+import vivisect.workspace as v_workspace
 import vivisect.lib.bexfile as v_bexfile
 import vivtestfiles.helpers as v_helpers
 
@@ -9,10 +10,43 @@ class HelloWorldTest(unittest.TestCase):
     def test_helloworld_i386_exe(self):
         with v_helpers.testfd('helloworld','hello_i386.exe') as fd:
             bex = v_bexfile.getBexFile(fd)
+
+            self.assertEqual( bex.info('arch'), 'i386' )
+            self.assertEqual( bex.info('format'), 'pe' )
+            self.assertEqual( bex.info('platform'), 'windows' )
+            self.assertEqual( bex.info('byteorder'), 'little' )
+
             self.assertEqual( bex.baseaddr(), 0x400000 )
+            self.assertIsNotNone( bex.section('.text') )
 
     def test_helloworld_amd64_exe(self):
         with v_helpers.testfd('helloworld','hello_amd64.exe') as fd:
             bex = v_bexfile.getBexFile(fd)
-            self.assertEqual( bex.baseaddr(), 0x140000000 )
 
+            self.assertEqual( bex.info('arch'), 'amd64' )
+            self.assertEqual( bex.info('format'), 'pe' )
+            self.assertEqual( bex.info('platform'), 'windows' )
+            self.assertEqual( bex.info('byteorder'), 'little' )
+
+            self.assertEqual( bex.baseaddr(), 0x140000000 )
+            self.assertIsNotNone( bex.section('.text') )
+
+    def test_helloworld_vw_i386(self):
+        with v_helpers.testfd('helloworld','hello_i386.exe') as fd:
+            bex = v_bexfile.getBexFile(fd)
+
+            vw = v_workspace.VivWorkspace()
+            vw.loadBexFile( bex )
+
+            view = vw.getVivView()
+            self.assertEqual( view.readMemory( bex.baseaddr(), 2 ), b'MZ' )
+
+    def test_helloworld_vw_amd64(self):
+        with v_helpers.testfd('helloworld','hello_amd64.exe') as fd:
+            bex = v_bexfile.getBexFile(fd)
+
+            vw = v_workspace.VivWorkspace()
+            vw.loadBexFile( bex )
+
+            view = vw.getVivView()
+            self.assertEqual( view.readMemory( bex.baseaddr(), 2 ), b'MZ' )
